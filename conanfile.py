1cdeb30b0a6e7033d6da34d1431a4f38d5599a75
memunix <jon@memunix.com>
2019-09-24 11:37:50 +0000

    removed old comments

--- conanfile.py
+++ conanfile.py
@@ -9,17 +9,10 @@ class CpphttplibConan(ConanFile):
     url = "https://github.com/yhirose/cpp-httplib"
     description = "A C++ single-file header-only cross platform HTTP/HTTPS library."
     # No settings/options are necessary, this is header only
-    # exports_sources = "include/*"
     no_copy_source = True
 
     def source(self):
         self.run("git clone https://github.com/yhirose/cpp-httplib")
-
-    # def source(self):
-    #     source_url = "https://github.com/yhirose/cpp-httplib"
-    #     tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version))
-    #     extracted_dir = self.name + "-" + self.version
-    #     os.rename(extracted_dir, "sources")
 
     def package(self):
         self.copy("*.h", dst="include", keep_path=False)
